5881c36203678fbe119f5cf20ed21ffb6883e9f4
Gabriele Icardi <redgabri@live.it>
2019-08-08 12:09:48 +0200

    addEntity() with a queue

--- Progetto api/Progetto api/Code.c
+++ Progetto api/Progetto api/Code.c
@@ -2,19 +2,37 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct Rel {
+	char name[64];
+	char receiver[128];
+	struct Rel* relPointer;
+} Rel;
+typedef Rel* RelPointer;
+
+typedef struct Entity {
+	char* name;
+	RelPointer relPointer;
+	struct Entity* entNext;
+} Entity;
+typedef Entity* EntityPointer;
+
 char* line=NULL;
+EntityPointer entHead = NULL;
+EntityPointer entRear = NULL;
 
 //Prototypes
 void readLine();
-void addEntity();
+EntityPointer addEntity();
 void addRelation();
 void printReport();
+/*		non servono per il primo test.
 void deleteEntity();
 void deleteRelation();
+*/
 
 
 void main(int argc, char* argv[]) {
-	line=(char*)malloc(64, sizeof(char));
+	line=(char*)malloc(64 * sizeof(char));
 	if (line == NULL) {
 		printf("Errore allocazione riga: ending process.");
 		return;
@@ -24,6 +42,7 @@ void main(int argc, char* argv[]) {
 		readLine();
 	}		//end while
 
+	return;
 }			//end of Main()
 
 
@@ -31,17 +50,37 @@ void main(int argc, char* argv[]) {
 void readLine() {
 	fgets(line, 7, stdin);
 	
-	if (!strcmp(line, "addent")) { addEntity(); }
-	else if (!strcmp(line, "addrel")) { addRelation(); }
+	if (!strcmp(line, "addent")) { 
+		if (entHead == NULL) {
+			entHead = addEntity();
+			entRear = entHead;
+		}
+		else entRear = addEntity();
+	}
+	else if (!strcmp(line, "addrel")) { addRelation();	}
 	else if (!strcmp(line, "report")) { printReport(); }
+	/*
 	else if (!strcmp(line, "delent")) { deleteEntity(); }
 	else if (!strcmp(line, "delrel")) { deleteRelation(); }
+	*/
 }
 
-void addEntity() {
-	fgets(line, 64, stdin);
-	printf("(addent) %s", line);
+EntityPointer addEntity() {
+	EntityPointer entPoint = (EntityPointer)malloc(sizeof(Entity));
+	if (entPoint != NULL) {
+		fgets(line, 64, stdin);
+		entPoint->relPointer = NULL;
+		entPoint->entNext = NULL;
+		entPoint->name = (char*)malloc(sizeof(char) * 64);
+		if (line != "") { strcpy_s(entPoint->name, 64, line); }
 
+		if (entHead != NULL) {
+			entRear->entNext = entPoint;
+		}
+		return entPoint;
+	}
+	else printf("Errore nell'allocazione di entPoint (addEntity): ending process.");
+	return NULL;
 }
 
 void addRelation() {
@@ -55,6 +94,7 @@ void printReport() {
 
 }
 
+/*
 void deleteEntity() {
 	fgets(line, 64, stdin);
 	printf("(delent)%s", line);
@@ -65,4 +105,5 @@ void deleteRelation() {
 	fgets(line, 64, stdin);
 	printf("(delrel)%s", line);
 
-}
+}		non servono per il primo test.
+*/
